e1f7603d2f6e1da2fb40e94371e4f27394f6ee58
Sujay149 <sujayss149@gmail.com>
2026-04-17 14:14:50 +0530

    refactor: remove serverless function entrypoint and update Vercel configuration for SSR

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Vercel serverless function entrypoint for TanStack Start SSR
-const { default: handler } = require('../dist/server/index.js');
-
-// Vercel expects a default export for Node.js API routes
-module.exports = handler;
